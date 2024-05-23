27a0087e517682cb54c0bb0352de9d1ad9c26c58
Maciej Włosek <wlomcj0@gmail.com>
2024-05-23 12:09:42 +0200

    text3-ifstatment

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const rollBtn = document.getElementById('rollBtn');
-const counter = document.getElementById('counter');
+const input1 = document.getElementById('input1');
+const button1 = document.getElementById('button1');
+const label1 = document.getElementById('label1');
+let age;
 
-
-rollBtn.onclick = function(){
-    const randNumber = Math.floor(Math.random() * 100)
-    counter.textContent = randNumber;
+button1.onclick = function() {
+    age = input1.value;
+    age = Number(age)
+if (age >= 0 && age <= 17) {
+    label1.textContent = `Masz za mało lat = ${age}`
+} else if (age >= 18 && age <=24){
+    label1.textContent = `Możesz przejść = ${age}`
+} else if (age >= 25) {
+    label1.textContent = `Jesesś za stary = ${age}`
+} else {
+    label1.textContent = `Wprowadz wiek`
+}
 }
