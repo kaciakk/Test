4028c75702a9a3a3189cf5d4f34f2a37c563582c
Maciej Włosek <wlomcj0@gmail.com>
2024-05-23 17:40:51 +0200

    test6-dice roller

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const rollBtn = document.getElementById('rollBtn');
-const counter = document.getElementById('counter');
+function rollDice() {
+    const input1 = document.getElementById('input1').value;
+    const result = document.getElementById('result');
+    const resultImg = document.getElementById('result-img');
+    const dicesRoll = [];
+    const dicesImage = [];
 
-
-rollBtn.onclick = function(){
-    const randNumber = Math.floor(Math.random() * 100)
-    counter.textContent = randNumber;
+   for ( let i = 0; i < input1; i++){
+      const randNumbers = Math.floor(Math.random() * 6) +1; 
+      dicesRoll.push(randNumbers);
+      dicesImage.push(`<img src="images/${randNumbers}.png" alt="Dice ${randNumbers}"}>`);
+      
+   }
+   result.textContent = dicesRoll.join(', ');
+   resultImg.innerHTML = dicesImage.join(' ');
 }
+
+rollDice();
