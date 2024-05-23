778247f8ade510688b0c6495a21c16ff4a0e10e2
Maciej Włosek <wlomcj0@gmail.com>
2024-05-23 14:30:25 +0200

    test 5

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-const input3 = document.getElementById('input3');
-const input4 = document.getElementById('input4');
-const p1 = document.getElementById('p1');
-const p2 = document.getElementById('p2');
-const button1 = document.getElementById('button1');
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+const result = document.getElementById("result");
+const button1 = document.getElementById("button1");
 
-
-button1.onclick = function(){
-    if(input1.checked){
-        p1.textContent = `You are subscribed`
-    } else {
-        p1.textContent = `You are not subscribed`
+button1.onclick = function() {
+    
+    calculate();
+    console.log(calculate());
     }
-    if(input2.checked){
-        p2.textContent = `You are paying with Blik`
-    } else if (input3.checked) {
-        p2.textContent = `You are paying with Paypal`
-    } else if (input4.checked){
-        p2.textContent = `You are paying with Visa`
-    } else {
-        p2.textContent = `Pick payement method`
+
+    function calculate() {
+      
+        if(input2.checked){
+            result.textContent = (input1.value * 1.8) + 32;
+        } else if (input3.checked){
+            result.textContent = (input1.value - 32) * 1.8;
+        } else 
+            result.textContent = `WYBIERZ JEDNSOTKE`
     }
-}
 
-
+    console.log(typeof(result))
 
