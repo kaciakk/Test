98253fe805cc4704e3524aa8974458b5e50aa882
Maciej Włosek <wlomcj0@gmail.com>
2024-05-24 17:26:08 +0200

    test8

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
+const display = document.getElementById('display')
+let timer = null;
+let starterTimer = 0;
+let elapsedTime = 0;
+let isRunning = false;
+function startTime(){
 
-function calculate(){
-   const input1 = document.getElementById('input1');
-   const input2 = document.getElementById('input2');
-   const input3 = document.getElementById('input3');
-   const p1 = document.getElementById('p1');
+   if(!isRunning){
+      startTime = Date.now() - elapsedTime;
+      timer = setInterval(update,10);
+      isRunning = true;
 
-   let principalAmount = input1.value;
-   let interestRate = input2.value;
-   let years = input3.value;
+   }
+}
+
+function stopTime(){
+   if(isRunning){
+      clearInterval(timer);
+      elapsedTime = Date.now() - startTime;
+      isRunning = false;
+   }
+
+}
+
+function resetTime(){
+   clearInterval(timer);
+   startTime = 0;
+   elapsedTime = 0;
+   isRunning = false;    
+   display.textContent = "00:00:00:00";
+
+}
+function update(){
+   const currentTime = Date.now();
+   elapsedTime = currentTime - startTime;
+
+   let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+   let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+   let seconds = Math.floor(elapsedTime / 1000 % 60);
+   let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+   hours = String(hours).padStart(2, "0");
+   minutes = String(minutes).padStart(2, "0");
+   seconds = String(seconds).padStart(2, "0");
+   milliseconds = String(milliseconds).padStart(2, "0");
+
+   display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
+
+   }
    
 
-   if(principalAmount < 0 ){
-     principalAmount = 0;
-   }
-   if(interestRate < 0 ){
-      interestRate = 0;
-    }
-    if(years < 0 ){
-      years = 0;
-    }
-    const result = principalAmount * Math.pow((1 + interestRate / 1), 1*years);
-   p1.textContent = result.toLocaleString('pl-PL', {
-      style:'currency',
-      currency:'PLN'
-    });
-}
