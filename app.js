035cf0ca092cc716fd28593e2e5c31a5fec2d723
Maciej Włosek <wlomcj0@gmail.com>
2024-05-24 17:44:04 +0200

    calculator

--- app.js
+++ app.js
@@ -1,51 +1,11 @@
 const display = document.getElementById('display')
-let timer = null;
-let starterTimer = 0;
-let elapsedTime = 0;
-let isRunning = false;
-function startTime(){
 
-   if(!isRunning){
-      startTime = Date.now() - elapsedTime;
-      timer = setInterval(update,10);
-      isRunning = true;
-
-   }
+function appendToDisplay(input) {
+   display.value += input;
 }
-
-function stopTime(){
-   if(isRunning){
-      clearInterval(timer);
-      elapsedTime = Date.now() - startTime;
-      isRunning = false;
-   }
-
+function clearDisplay() {
+   display.value = '';
 }
-
-function resetTime(){
-   clearInterval(timer);
-   startTime = 0;
-   elapsedTime = 0;
-   isRunning = false;    
-   display.textContent = "00:00:00:00";
-
+function calculate(){
+   display.value = eval(display.value)
 }
-function update(){
-   const currentTime = Date.now();
-   elapsedTime = currentTime - startTime;
-
-   let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-   let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
-   let seconds = Math.floor(elapsedTime / 1000 % 60);
-   let milliseconds = Math.floor(elapsedTime % 1000 / 10);
-
-   hours = String(hours).padStart(2, "0");
-   minutes = String(minutes).padStart(2, "0");
-   seconds = String(seconds).padStart(2, "0");
-   milliseconds = String(milliseconds).padStart(2, "0");
-
-   display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
-
-   }
-   
-
