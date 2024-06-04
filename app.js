51633c453139b1c0c13d65bc4d19454e7ba61598
Maciej Włosek <wlomcj0@gmail.com>
2024-06-04 22:32:20 +0200

    test11

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
-const display = document.getElementById('display')
 
-function appendToDisplay(input) {
-   display.value += input;
-}
-function clearDisplay() {
-   display.value = '';
-}
-function calculate(){
-   display.value = eval(display.value)
-}
+   function getComputerChoice(){
+      let randNum = Math.random();
+      if(randNum <= 0.33333333){
+         return "kamien";
+      } else if(randNum > 0.333334 && randNum <= 0.666666){
+         return "papier";
+      } else {
+         return "nozyce";
+      }
+   }
+   
+   function getHumanChoice(){
+      let sign = prompt("What's your sign?")
+      if(sign.toLowerCase() === 'kamien'){
+         console.log("wybrałes Kamien");
+         return "kamien";
+      } else if(sign.toLowerCase() === "papier"){
+         console.log("wybrałes papier")
+         return "papier";
+      } else if(sign.toLowerCase() === "nozyce"){
+         console.log("wybrałes nozyce")
+         return "nozyce";
+      }
+   }
+   
+   const humanSelection = getHumanChoice();
+   const computerSelection = getComputerChoice();
+   let humanScore = 0;
+   let computerScore = 0;
+   
+   function playRound(humanChoice, computerChoice) {
+      if(humanChoice === computerChoice){
+         console.log("remis")
+      }else if (humanChoice === "kamien" && computerChoice === "papier"){
+         console.log("kamień przegrywa z papierem. komputer wygrywa")
+         computerScore++
+      }else if (humanChoice === "kamien" && computerChoice === "nozyce"){
+         console.log("kamień wygrywa z nozyce. uzytkownik wygrywa")
+         humanScore++
+    }else if (humanChoice === "papier" && computerChoice === "nozyce"){
+      console.log("papier przegrywa z nozyce. uzytkownik przegrywa")
+      computerScore++
+   }else if (humanChoice === "papier" && computerChoice === "kamien"){
+      console.log("papier wyrgywa z kamien. uzytkownik wygrywa")
+       humanScore++
+   }else if (humanChoice === "nozyce" && computerChoice === "kamien"){
+      console.log("nozyce przegrywa z kamien. uzytkownik przgrywa")
+      computerScore++
+   }else if (humanChoice === "nozyce" && computerChoice === "papier"){
+      console.log("papier wyrgywa z kamien. uzytkownik wygrywa")
+       humanScore++
+   }
+   
+   }
+ 
+
+
+
